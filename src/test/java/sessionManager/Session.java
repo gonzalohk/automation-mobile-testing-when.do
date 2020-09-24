e3760502a55c44131d4134a2b0c379de03f0b327
gonzalohk <goh.acm@gmail.com>
2020-09-24 16:06:21 -0400

    sessionManager was added

--- src/test/java/sessionManager/Session.java
+++ src/test/java/sessionManager/Session.java
@@ -1,8 +1,30 @@
 package sessionManager;
 
+import deviceFactory.FactoryDevice;
+import io.appium.java_client.AppiumDriver;
+
+import java.net.MalformedURLException;
+
 public class Session {
     public static Session session= null;
-    private Session() {
+    private AppiumDriver driver;
+    private Session() throws MalformedURLException {
+        driver = FactoryDevice.make("android").create();
+    }
 
+    public static Session getInstance(){
+        if (session == null){
+            session = new Session();
+        }
+        return session;
+    }
+
+    public void closeSession (){
+        driver.quit();
+        session = null;
+    }
+
+    public AppiumDriver getDriver(){
+        return driver;
     }
 }
